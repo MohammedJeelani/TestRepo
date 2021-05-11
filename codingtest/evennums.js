be891a318c499e034a6bf76f852543f1b3541eb3
MohammedJeelani <68697778+MohammedJeelani@users.noreply.github.com>
2021-05-11 13:36:47 +0530

    Add files via upload

--- codingtest/evennums.js
+++ codingtest/evennums.js
@@ -1,1 +1,14 @@
+// Even numbers in an Array //
 
+let numArray = [2 , 15 , 38, 50, 101, 199, 333, 500];
+
+for(var i = 0 ; i <= numArray.length ; i++){
+    if(numArray[i] % 2 == 0){
+       console.log(numArray[i]);
+    }
+    
+}
+
+
+
+    
